2631e448d89d32f1bad8dc30d118f1be91e85165
Nitin Pal <nitinluck0123@gmail.com>
2019-07-08 16:37:50 +0530

    Pulled Helper class for github

--- app/src/main/java/nitin/luckyproject/notemaster/NoteActivity.java
+++ app/src/main/java/nitin/luckyproject/notemaster/NoteActivity.java
@@ -1,5 +1,6 @@
 package nitin.luckyproject.notemaster;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,17 +12,52 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
 
+    public static final String NOTE_INFO ="nitin.luckyproject.notemaster.NOTE_INFO";
+    private NoteInfo mNote;
+    private boolean mIsNewNote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Spinner spinnercourse = findViewById(R.id.spinner_course);
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses =
+                new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnercourse.setAdapter(adapterCourses);
 
 
+        readDisplayStateValues();
+
+        EditText textNoteTitle =  findViewById(R.id.note_title);
+        EditText textNoteText =   findViewById(R.id.note_desc);
+
+        if(!mIsNewNote)
+            displayNote(spinnercourse, textNoteTitle, textNoteText);
+
+
+    }
+    private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        int courseIndex = courses.indexOf(mNote.getCourse());
+        spinnerCourses.setSelection(courseIndex);
+        textNoteTitle.setText(mNote.getTitle());
+        textNoteText.setText(mNote.getText());
+    }
+    private void readDisplayStateValues() {
+        Intent intent = getIntent();
+        mNote = intent.getParcelableExtra(NOTE_INFO);
+        mIsNewNote = mNote == null;
     }
 
     @Override
--- app/src/main/java/nitin/luckyproject/notemaster/NoteListActivity.java
+++ app/src/main/java/nitin/luckyproject/notemaster/NoteListActivity.java
@@ -32,6 +32,7 @@ public class NoteListActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                startActivity(new Intent(NoteListActivity.this,NoteActivity.class));
             }
         });
 
@@ -40,25 +41,21 @@ public class NoteListActivity extends AppCompatActivity {
 
     private void initializeDisplayContent() {
 
-        ListView listview = findViewById(R.id.lists_note);
-        ArrayList<String> list =new ArrayList<>();
-        list.add("first line ");
-        list.add("second line");
-        list.add("third line ");
-        list.add("four line");
-        list.add("five line ");
-        list.add("six line");
-        list.add("seven line ");
-        list.add("eight line");
+        final ListView listNotes =  findViewById(R.id.lists_note);
 
-        ArrayAdapter<String> arrayAdapter =new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
-        listview.setAdapter(arrayAdapter);
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+
+        listNotes.setAdapter(adapterNotes);
+
         //setting onItemClickListener
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent =new Intent(NoteListActivity.this,NoteActivity.class);
+                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(i);
+                intent.putExtra(NoteActivity.NOTE_INFO,note);
                 startActivity(intent);
 
             }
